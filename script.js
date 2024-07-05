56561da4eb7d73b16c7e64e18b1567f6f7eba5e9
Victor Adewale <afuwapeadewale9@gmail.com>
2024-07-05 14:56:03 +0100

    Add files via upload
    Added the images

---
+++ script.js
@@ -0,0 +1,18 @@
+document.querySelectorAll('a').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        const targetId = this.getAttribute('href').substring(1);
+        const targetElement = document.getElementById(targetId);
+
+        window.scrollTo({
+            top: targetElement.offsetTop,
+            behavior: 'smooth'
+        });
+    });
+});
+
+
+function scrollToTop(){
+    window.scrollTo(0, 0);
+}bn
